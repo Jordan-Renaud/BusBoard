f7f8b60f653e3a2e2a8aa38585151d2c1ca7d4a1
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-04-28 15:48:45 +0100

    request postcode

--- busBoard.mjs
+++ busBoard.mjs
@@ -1,4 +1,5 @@
 import fetch from "node-fetch";
+import readLine from "readline";
 
 const stopID = "490008660N";
 const url = `https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals`;
@@ -20,3 +21,39 @@ arrivals.forEach((busArrival) => {
     );
   }
 });
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let postcode;
+
+rl.question("What is your postcode? ", function (answer) {
+  console.log(`Postcode confirmation: ${answer}`);
+  postcode = answer;
+  getPostcodeData(postcode);
+  rl.close();
+});
+
+function getPostcodeData(postcode) {
+  const urlForPostCodeRequest = `https://api.postcodes.io/postcodes/${postcode}`;
+  const input = await fetch(urlForPostCodeRequest);
+  const postcodeData = await input.json();
+  return postcodeData;
+}
+
+
+//current errors
+//we need to wait for user to put in postcode.
+//
+
+//get users input for postcode
+//remove any spaces + captilise it
+//get long and lat
+
+//url = api.postcodes.io/postcodes/{postcode}
+//validate => if status 404 then inform user
+//retrieve longitude + latitude
+//coords for testing = lat = 51.55411 long = -0.292968
+// url for bus stops within long + lat = https://api.tfl.gov.uk/StopPoint/?lat={lat}&lon={lon}&stopTypes={stopTypes}[&radius][&useStopPointHierarchy][&modes][&categories][&returnLines]
