e6ced4a9b11ec264f026dd86d2fda2d17ed3a4dc
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-04-28 12:15:33 +0100

    log out bus arrival times

--- busBoard.mjs
+++ busBoard.mjs
@@ -1,12 +1,22 @@
 import fetch from "node-fetch";
 
-const apiKey = "";
 const stopID = "490008660N";
-const url = `https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals?app_key=${apiKey}`;
+const url = `https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals`;
 
 const response = await fetch(url);
 const arrivals = await response.json();
 
+//sort arrivals with time to station
+arrivals.sort((a, b) => a.timeToStation - b.timeToStation);
+
 arrivals.forEach((busArrival) => {
-  console.log(busArrival);
+  const minutesUntilBusArrives = Math.floor(busArrival.timeToStation / 60);
+
+  if (minutesUntilBusArrives === 0) {
+    console.log(`Bus to ${busArrival.destinationName} is due.`);
+  } else {
+    console.log(
+      `Bus to ${busArrival.destinationName} arriving in ${minutesUntilBusArrives} minutes.`
+    );
+  }
 });
